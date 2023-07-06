8276708cfb80745f50671b7992128bd7437bb543
sky6565 <godwin1988b@outlook.com>
2023-07-05 22:53:13 -0700

    first commit

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var passwordLength = prompt("How many characters in your password?");
+var includeLowercase = confirm("Do you want lowercase letters?");
+var includeuppercase = confirm("Do you want uppercase?");
+var includenumbers = confirm("Do you want to include numbers ?");
+var includesymbols = confirm ("Do you want to include symbols ?"); 
+
+  
+
+
+
+
+
+// Generator funtions - http://www.net-comber.com/charset.html
+
+ function getRandomLower(p) { 
+  return String.fromCharCode(math.floor(math.random() * 26) + 97);
+ }
