2405f2545e236ef433750f24b9b3617083a7fac9
sky6565 <godwin1988b@outlook.com>
2023-07-16 03:11:50 -0700

    ffinished-code

--- script.js
+++ script.js
@@ -10,22 +10,60 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate buttong
+function generatePassword () {
+  var newPassword = ""
+
+
+
+  var passwordLength = parseInt( prompt("How many characters in your password?"));
+  var includeLowercase = confirm("Do you want lowercase letters?");
+  var includeuppercase = confirm("Do you want uppercase?");
+  var includenumbers = confirm("Do you want to include numbers ?");
+  var includesymbols = confirm ("Do you want to include symbols ?"); 
+
+  if (passwordLength > 128 || passwordLength < 8 ) {
+    return "Invalid password lenght"
+  }
+
+  var bank = []
+  var lowercase = ["abcdefghijklmnopqrstuvwxyz"]
+  var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  var numbers = ["0123456789"]
+  var symbols = ["`~@#$%^&*()-_=+{[|\"':;>.<,?/!]}`"] 
+
+
+  if (includeLowercase ) {
+    bank = bank + lowercase
+  }
+
+  if (includeuppercase  ) {
+    bank = bank + uppercase
+  }
+
+  if (includenumbers  ) {
+    bank = bank + numbers
+  }
+ 
+  if (includesymbols  ) {
+    bank = bank + symbols
+  }
+ console.log ("BANK: ", bank)
+  var length = bank.length
+  for (i=0;i < passwordLength ; i++) {
+    var random = Math.floor (Math.random ()*length)
+    var randomcharacter= bank [random]
+   
+    newPassword = newPassword + randomcharacter
+    console.log (newPassword)
+
+
+  }
+
+
+  // Generator funtions - http://www.net-comber.com/charset.html
+
+    return newPassword
+
+}
 generateBtn.addEventListener("click", writePassword);
-var passwordLength = prompt("How many characters in your password?");
-var includeLowercase = confirm("Do you want lowercase letters?");
-var includeuppercase = confirm("Do you want uppercase?");
-var includenumbers = confirm("Do you want to include numbers ?");
-var includesymbols = confirm ("Do you want to include symbols ?"); 
-
-  
-
-
-
-
-
-// Generator funtions - http://www.net-comber.com/charset.html
-
- function getRandomLower(p) { 
-  return String.fromCharCode(math.floor(math.random() * 26) + 97);
- }
